828baff6b8d3cebfbdef5f6eae5566e48bf55da7
Bob <bobbaicloudwithpants@gmail.com>
2023-02-22 12:37:40 -0800

    correct the run_test.sh script

--- run_test.sh
+++ run_test.sh
@@ -3,6 +3,6 @@ rm -rf ${OUT_DIR}
 
 SRC_DIR=src
 mkdir -p ${OUT_DIR}
-javac -d ${OUT_DIR} -cp ".:lib/*" ${SRC_DIR}/StatusCode.java ${SRC_DIR}/Table.java ${SRC_DIR}/TableManager.java ${SRC_DIR}/TableManagerImpl.java ${SRC_DIR}/TableManagerTest.java
+javac -d ${OUT_DIR} -cp ".:lib/*" ${SRC_DIR}/StatusCode.java ${SRC_DIR}/TableMetadata.java ${SRC_DIR}/TableManager.java ${SRC_DIR}/TableManagerImpl.java ${SRC_DIR}/TableManagerTest.java
 java -cp "${OUT_DIR}:./lib/*" org.junit.runner.JUnitCore TableManagerTest
 
